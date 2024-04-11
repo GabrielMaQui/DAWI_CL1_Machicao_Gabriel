91c0b9493f026ece7349fd0d60b0c0ca5f409bb3
Gabriel <gmachicaoq@unsa.edu.pe>
2024-04-11 12:31:43 -0500

    Iniciando el proyecto de la evalucacion

---
+++ src/main/java/cibertec/com/pe/primera_evaluacion/PrimeraEvaluacionApplication.java
@@ -0,0 +1,13 @@
+package cibertec.com.pe.primera_evaluacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeraEvaluacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeraEvaluacionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cibertec/com/pe/primera_evaluacion/PrimeraEvaluacionApplicationTests.java
@@ -0,0 +1,13 @@
+package cibertec.com.pe.primera_evaluacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeraEvaluacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
