6092d00beb11762c535d037fd80cb42b24c5a558
Gabriel <gmachicaoq@unsa.edu.pe>
2024-04-11 22:52:01 -0500

    Se agrega la pagina html donde se muestra el formulario y se hace el post mas el metodo para determinar si es bisiesto

--- src/main/java/cibertec/com/pe/primera_evaluacion/controller/YearController.java
+++ src/main/java/cibertec/com/pe/primera_evaluacion/controller/YearController.java
@@ -4,6 +4,8 @@ import cibertec.com.pe.primera_evaluacion.model.YearModel;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.time.Year;
 
@@ -17,4 +19,19 @@ public class YearController {
         model.addAttribute("mostrarMensaje", false);
         return "bisiesto";
     }
+
+    @PostMapping("/bisiesto")
+    public String bisiesto(@ModelAttribute("bisiesto") YearModel yearModel, Model model) {
+        model.addAttribute("titulo", "Examinador de Bisiestos");
+        model.addAttribute("mostrarMensaje", true);
+
+        if(yearModel.getYear() % 4 == 0 && yearModel.getYear() % 100 != 0 || yearModel.getYear() % 400 == 0 ){
+            model.addAttribute("resultado", "Es un año Bisiesto");
+        }
+        else {
+            model.addAttribute("resultado", "No es un año Bisiesto");
+        }
+        return "bisiesto";
+    }
+
 }
